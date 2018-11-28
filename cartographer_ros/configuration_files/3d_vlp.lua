fe8790d2730c3d79d5ba1fb39b61fbde6532fea3
jo679513 <40849477+jo679513@users.noreply.github.com>
2018-11-28 10:05:54 +0800

    Add files via upload

--- cartographer_ros/configuration_files/3d_vlp.lua
+++ cartographer_ros/configuration_files/3d_vlp.lua
@@ -47,9 +47,13 @@ TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 2
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
 POSE_GRAPH.optimization_problem.huber_scale = 5e2
-POSE_GRAPH.optimize_every_n_nodes = 0
+POSE_GRAPH.optimize_every_n_nodes = 320
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
 POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
 POSE_GRAPH.constraint_builder.min_score = 0.62
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 0.5
+POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 3
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 3
+POSE_GRAPH.optimization_problem.odometry_rotation_weight = 0.5
 return options
